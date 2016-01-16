67e8899fc2d101af67f4b5f5802ec3fc5da588e6
Maciej Romanowski <mac.romanowski@gmail.com>
2016-01-16 22:49:08 +0100

    Reverting accidental committed files to proper ones

--- serenity_pypeline/pca_executor.py
+++ serenity_pypeline/pca_executor.py
@@ -1,5 +1,4 @@
 from filter_step import FilterStep
-import numpy as np
 
 __author__ = 'MacRomanowski'
 
@@ -7,10 +6,7 @@ __author__ = 'MacRomanowski'
 class PcaExecutor(FilterStep):
 
     def __init__(self, conf):
-        super(FilterStep, self).__init__(conf)
+        super(FilterStep, self).__init__()
 
     def run(self, **kwargs):
-        print 'Counting PCA...'
-
-    def _calculate_corr_matrix(self, input_matrix):
-        return np.corrcoef(input_matrix)
+        print 'Executing: PcaExecutor'
