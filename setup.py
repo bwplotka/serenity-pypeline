51b5100b9f3b081dbb8909c98d4934a054375b8a
Maciej Romanowski <mac.romanowski@gmail.com>
2016-01-16 13:41:02 +0100

    Adding project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='serenity-pypeline',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='Apache License 2.0',
+    author='plotkaba, macromanowski, dejson',
+    author_email='',
+    description='Serenity Python Pipeline'
+)
