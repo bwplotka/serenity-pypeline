0c269c61fa927fb9294758645355a8696b7d5988
Bartlomiej Plotka <bartlomiej.plotka@intel.com>
2016-02-10 14:27:22 +0100

    Added more packages as dependencies.
    Signed-off-by: Bartlomiej Plotka <bartlomiej.plotka@intel.com>

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name='serenity-pypeline',
     version='1.0',
-    packages=['serenity-pypeline'],
+    packages=['serenity_pypeline'],
     url='',
     license='Apache License 2.0',
     author='plotkaba, macromanowski, dejson',
     author_email='',
-    description='Serenity Python Pipeline', requires=['influxdb', 'sqlbuilder', 'protobuf']
+    description='Serenity Python Pipeline', requires=['influxdb', 'sqlbuilder', 'protobuf', 'numpy']
 )
